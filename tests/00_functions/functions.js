8752ddf67e552b70d34c81f2cc482a71b2406b8c
asiddo <siddo21@msn.com>
2022-12-12 15:05:24 -0500

    00_functions Homework completed

--- tests/00_functions/functions.js
+++ tests/00_functions/functions.js
@@ -1,19 +1,63 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable consistent-return */
-function max() {
-
+function max(num1, num2, num3) { 
+let maxNum;
+if (num1 >=num2) {
+  maxNum = num1;}
+else {
+  maxNum = num2;}
+if (maxNum < num3) {
+  maxNum = num3;
+}
+return maxNum;
 }
 
-function addUnknown() {
-
+function addUnknown(num1, num2,num3,num4, num5) {
+ let sumNum = num1 + num2;
+ if (num3 != null){
+   sumNum += num3;
+}
+if (num4 != null){
+  sumNum += num4;
+}
+if (num5 != null){
+  sumNum += num5;
+}
+    return sumNum;
 }
 
-function evenOrOdd() {
-
+function evenOrOdd(num) {
+  let oddEven;
+  if(num % 2 === 0){
+    oddEven = "even";
+  }
+  else{
+    oddEven = "odd";
+  }
+  return oddEven;
 }
 
-function operator() {
+function operator(num1, num2, num3) {
+let plusMinus;
+if(num3 === num1 + num2){
+  plusMinus = "plus";
+}
+else if (num3 === num1 - num2){
+  plusMinus = "minus";
 
+}
+else if (num3 === num1 * num2){
+  plusMinus = "multiply";
+
+}
+else if (num3 === num1 / num2){
+  plusMinus = "divide";
+
+}
+else {
+  plusMinus = null;
+}
+return plusMinus;
 }
 
 module.exports = {
