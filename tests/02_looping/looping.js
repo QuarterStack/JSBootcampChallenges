6d6e4670326fda0dca59860362626502d2904ffb
asiddo <siddo21@msn.com>
2022-12-16 17:41:08 -0500

    02_Looping Homework completed

--- tests/02_looping/looping.js
+++ tests/02_looping/looping.js
@@ -1,20 +1,47 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 function factorial(num) {
-
+  let Total = 1;
+  for(let index = num; index > 0 ;index--){
+      Total = Total * index;
+  }
+  return Total;
+  
 }
 
 function stringReverse(str) {
+  const strLength = str.length;
+  let reverseString = '';
+
+  for (let index = strLength - 1; index >= 0; index-- ){
+    reverseString += str[index];
+  }
+  return(reverseString);
 
 }
 
 // do not use built in slice method.
 function slicer(originalString, startIdx, endIdx) {
+  if (startIdx === undefined && endIdx === undefined ){
+    return originalString;
+ }
+ else if (endIdx === undefined){
+    return(originalString.substr(startIdx));
+ }
+ else if (startIdx != undefined && endIdx !=undefined ){
+   return(originalString.substring(startIdx,endIdx));  
+ }
 
 }
 
 function addTheEvens(num) {
-
+  let total = 0;
+  for(let index = num; index >=0;index --){
+    if(index % 2 === 0){
+      total += index;
+    }
+  }
+  return total;
 }
 
 module.exports = {
